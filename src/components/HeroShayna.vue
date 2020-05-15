50ed519ae8cce70e3366baaea4bfe5f1837ab76c
Nico Dwi Kuswanto <nicodwika@gmail.com>
2020-05-15 15:45:58 +0700

    upload projek

---
+++ src/components/HeroShayna.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="single-hero-items set-bg" >
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <span>Bag,kids</span>
+                    <h1>Black friday</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    </p>
+                    <a href="#" class="primary-btn">Shop Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeroShayna'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+        <!-- Page Preloder -->
+    <!-- <div id="preloder">
+        <div class="loader"></div>
+    </div> -->
+    <HeaderShayna></HeaderShayna>
+
+        <!-- Hero Section Begin -->
+    <section class="hero-section">
+        <carousel class="hero-items" :items=1 :nav=false :mouseDrag=true>
+            <HeroShayna style="background-image: url('img/hero-1.jpg')"/>
+            <HeroShayna style="background-image: url('img/hero-2.jpg')"/>
+            <HeroShayna style="background-image: url('img/hero-3.jpg')"/>
+        </carousel>
+    </section>
+    <SectionShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+    <!-- Hero Section End --> 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import carousel from 'vue-owl-carousel'
+import HeroShayna from '@/components/HeroShayna.vue';
+import SectionShayna from '@/components/SectionShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerShayna from '@/components/PartnerShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    carousel,
+    HeroShayna,
+    SectionShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
